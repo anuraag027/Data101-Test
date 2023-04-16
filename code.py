98666979c42bc5726564b202afcd5775f59a4dd3
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-04-16 01:00:51 -0400

    Update code.py

--- code.py
+++ code.py
@@ -34,7 +34,7 @@ if st.button("Generate"):
 
     st.download_button(
   label="Download data as CSV",
-  data=pd.read_csv("airbnb_questions.csv"),#df.to_csv("airbnb_questions.csv", index=False).encode('utf-8'),
+  data=data=df.to_csv(index=False).encode('utf-8'),#pd.read_csv("airbnb_questions.csv"),#df.to_csv("airbnb_questions.csv", index=False).encode('utf-8'),
   file_name='output.csv',
   mime='text/csv',
 )
