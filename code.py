f162769c7920e95b1ee4f955e12b19888cb84d77
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-04-16 00:09:52 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,36 @@
+!pip install pandas
+!pip install streamlit
+
+#Import the necessary libraries and load the airbnb dataset:
+
+import pandas as pd
+import streamlit as st
+
+airbnb = pd.read_csv("https://raw.githubusercontent.com/dev7796/data101_tutorial/main/files/dataset/airbnb.csv")
+
+#Define a function to generate questions based on the template:
+
+def generate_questions(num_questions):
+    questions = []
+    for i in range(num_questions):
+        agg = random.choice(['min', 'max', 'mean'])
+        attr = random.choice(['price'])
+        cat_attrs = random.sample(list(airbnb.select_dtypes(include=['object']).columns), random.randint(1, 3))
+        conditions = []
+        for cat_attr in cat_attrs:
+            value = random.choice(airbnb[cat_attr].unique())
+            conditions.append(f"{cat_attr}='{value}'")
+        query = " and ".join(conditions)
+        question = f"What is the {agg} {attr} when {query}"
+        questions.append(question)
+    return questions
+
+st.title("Airbnb Questions Generator")
+num_questions = st.number_input("Enter the number of questions to generate", min_value=1, max_value=1000, value=10)
+if st.button("Generate"):
+    questions = generate_questions(num_questions)
+    st.write("Generated questions:")
+    st.write(questions)
+    # write questions to a CSV file
+    df = pd.DataFrame(questions, columns=["Question"])
+    df.to_csv("airbnb_questions.csv", index=False)
