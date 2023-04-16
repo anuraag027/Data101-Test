b0eb28027f90ef273ef201c15f39ed30184d0eac
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-04-16 00:25:15 -0400

    Update code.py

--- code.py
+++ code.py
@@ -29,7 +29,7 @@ if st.button("Generate"):
     st.write(questions)
     # write questions to a CSV file
     df = pd.DataFrame(questions, columns=["Question"])
-    csv = df.to_csv("airbnb_questions.csv", index=False).encode("utf-8")
+    csv = df.to_csv("airbnb_questions.csv", index=False)#.encode("utf-8")
     st.dataframe(pd.read_csv("airbnb_questions.csv"))#,height=300)
 
     st.download_button(
